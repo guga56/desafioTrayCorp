8f668c595edccd7711c424ae58737a9c525ae7ad
guga56 <gustavogregorio086@gmail.com>
2022-03-18 23:03:20 -0300

    criação do projeto

---
+++ Desafio.Dominio/Dominio/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Dominio.Dominio
+{
+    [Table("tb_Produto")]
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Estoque { get; set; }
+
+        public int Valor { get; set; }
+    }
+}
---
+++ Desafio.Infraestruture/Infraestruture/ProdutoContexto.cs
@@ -0,0 +1,27 @@
+﻿using Desafio.Dominio.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Infraestruture.Infraestruture
+{
+    public class ProdutoContexto : DbContext
+    {
+
+        public ProdutoContexto()
+        {
+
+        }
+
+        public ProdutoContexto(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
